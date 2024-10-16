2ba23a96239d69a03b19e4882830da4bf8bec912
ahmdWard <ahmdemadabdallah@gmail.com>
2024-10-16 19:52:46 +0300

    adding async fun and error handling middleware

---
+++ middlewares/errorMiddleWare.js
@@ -0,0 +1,20 @@
+const AppError= require('../utils/appError')
+
+const sendErrorDev=(err,res) => res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+})
+const globalErrors = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+    err.message = err.message || 'internal server error';
+
+   if(process.env.NODE_ENV !== 'production'){
+         sendErrorDev(err,res);
+   }
+}
+
+module.exports = globalErrors;
+
