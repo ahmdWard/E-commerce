cfba1c8e7ee9a8f7270366c430b853ad30eb6b5f
ahmdWard <ahmdemadabdallah@gmail.com>
2024-10-16 19:55:56 +0300

    adding morgan and nodeEnv and basic routes to app

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express= require('express')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const globalErrors = require('./middlewares/errorMiddleWare')
+const productRouter= require('./routes/productRoutes')
+const userRouter = require('./routes/userRoutes')
+
 
 const app =express()
 
 app.use(express.json())
 
+
+const NODE_ENV = process.env.NODE_ENV
+if (NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+    console.log(`mode ${NODE_ENV}`);
+}
+
+app.use('/api/v1/product',productRouter)
+app.use('/api/v1/user',userRouter)
+
+app.use(globalErrors)
 
 module.exports=app
