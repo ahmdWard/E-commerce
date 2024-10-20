2faa439bac5fbebf9e075b12441508f60f105f4e
ahmdWard <ahmdemadabdallah@gmail.com>
2024-10-20 12:41:11 +0300

    adding crud operation to users

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,3 +25,39 @@ exports.getAllUsers = catchAsync(async(req,res,next)=>{
         }
     })
 })
+
+
+exports.getUser = catchAsync(async(req,res,next)=>{
+
+    const user = await userModel.findById(req.params.id)
+
+    res.status(200).json({
+        status:"success",
+        data:{
+            user
+        }
+    })
+})
+
+exports.updateUser= catchAsync(async(req,res,next)=>{
+
+    const user = await userModel.findByIdAndUpdate(req.params.id,req.body,{
+        new:true
+    })
+
+    res.status(200).json({
+        status:"success",
+        data:{
+            user
+        }
+    })
+})
+
+exports.deleteUser= catchAsync(async(req,res,next)=>{
+
+     await userModel.findByIdAndDelete(req.params)
+
+    res.status(204).json({
+        status:"success"
+    })
+})
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,5 +7,11 @@ router
 .get(userController.getAllUsers)
 .post(userController.createUser)
 
+router
+.route('/id:')
+.get(userController.getUser)
+.delete(userController.deleteUser)
+.patch(userController.updateUser)
+
 
  module.exports= router
